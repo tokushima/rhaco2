d2fce19bb7eb3e63eec8986a7679567414eda394
tokushima <rhaco1004@gmail.com>
2011-11-20 03:37:55 +0900

    古いbinをパッケージング Developerのリニューアル

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php require dirname(__FILE__)."/rhaco2_core.php"; app(); ?>
-<app nomatch_redirect="/">
-	<handler>
-		<map url="" redirect="dev" />
+<app nomatch_redirect="/" name="rhaco2 repository" summary="rhaco2のライブラリ群">
+	<description>
+		さまざまなライブラリ
+		これまでもこれからも
+	</description>
+	<handler class="org.rhaco.flow.parts.Developer" url="">
+		<module class="org.rhaco.flow.module.TwitterBootstrapPagination" />
 	</handler>
-	<handler class="org.rhaco.flow.parts.Developer" url="dev">
+	<handler>
+		<map url="aaa" template="index.html" />
 	</handler>
 </app>
