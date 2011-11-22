c55ed7508fcdfc4190680062307df5fbaffb3ccc
tokushima <rhaco1004@gmail.com>
2011-11-23 01:36:06 +0900

    テスト関数 b, xmlの追加

--- bin/rhaco2.php
+++ bin/rhaco2.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 20111121
+ * @version 20111123
  */
 /**
  * アプリケーション定義
@@ -9029,107 +9029,150 @@ function deepcopy($var){
 function pwd(){
 	return str_replace("\\",'/',getcwd());
 }
-/**
- * $resultが$expectationが同じである事を検証する
- * @param mixed $expectation なるべき値
- * @param mixed $result テスト対象の値
- * @return boolean
- */
-function eq($expectation,$result){
-	list($debug) = debug_backtrace(false);
-	return Test::equals($expectation,$result,true,$debug["line"],$debug["file"]);
+if(!function_exists('eq')){
+	/**
+	 * $resultが$expectationが同じである事を検証する
+	 * @param mixed $expectation なるべき値
+	 * @param mixed $result テスト対象の値
+	 * @return boolean
+	 */
+	function eq($expectation,$result){
+		list($debug) = debug_backtrace(false);
+		return Test::equals($expectation,$result,true,$debug["line"],$debug["file"]);
+	}
 }
-/**
- * 成功とする
- * @return boolean
- */
-function success(){
-	list($debug) = debug_backtrace(false);
-	return Test::equals(true,true,true,$debug["line"],$debug["file"]);
+if(!function_exists('success')){
+	/**
+	 * 成功とする
+	 * @return boolean
+	 */
+	function success(){
+		list($debug) = debug_backtrace(false);
+		return Test::equals(true,true,true,$debug["line"],$debug["file"]);
+	}
 }
-/**
- * 失敗
- * @return boolean
- */
-function fail(){
-	list($debug) = debug_backtrace(false);
-	return Test::equals(false,true,true,$debug["line"],$debug["file"]);
+if(!function_exists('fail')){
+	/**
+	 * 失敗
+	 * @return boolean
+	 */
+	function fail(){
+		list($debug) = debug_backtrace(false);
+		return Test::equals(false,true,true,$debug["line"],$debug["file"]);
+	}
 }
-/**
- *　文字列中に指定した文字列がすべて存在していれば成功
- * @param string $keyword スペース区切りで複数可能
- * @param string $src
- * @return boolean
- */
-function meq($keyword,$src){
-	list($debug) = debug_backtrace(false);
-	if(empty($keyword)) throw new InvalidArgumentException("undef keyword");
-	return Test::equals(true,Text::match($src,$keyword),true,$debug["line"],$debug["file"]);
+if(!function_exists('meq')){
+	/**
+	 *　文字列中に指定した文字列がすべて存在していれば成功
+	 * @param string $keyword スペース区切りで複数可能
+	 * @param string $src
+	 * @return boolean
+	 */
+	function meq($keyword,$src){
+		list($debug) = debug_backtrace(false);
+		if(empty($keyword)) throw new InvalidArgumentException("undef keyword");
+		return Test::equals(true,Text::match($src,$keyword),true,$debug["line"],$debug["file"]);
+	}
 }
-/**
- *　文字列中に指定した文字列がすべて存在していなければ成功
- * @param string $keyword スペース区切りで複数可能
- * @param string $src
- * @return boolean
- */
-function nmeq($keyword,$src){
-	list($debug) = debug_backtrace(false);
-	return Test::equals(false,Text::match($src,$keyword),true,$debug["line"],$debug["file"]);
+if(!function_exists('nmeq')){
+	/**
+	 *　文字列中に指定した文字列がすべて存在していなければ成功
+	 * @param string $keyword スペース区切りで複数可能
+	 * @param string $src
+	 * @return boolean
+	 */
+	function nmeq($keyword,$src){
+		list($debug) = debug_backtrace(false);
+		return Test::equals(false,Text::match($src,$keyword),true,$debug["line"],$debug["file"]);
+	}
 }
-/**
- * $resultが$expectationが同じではない事を検証する
- * @param mixed $expectation ならないはずの値
- * @param mixed $result テスト対象の値
- * @return boolean
- */
-function neq($expectation,$result){
-	list($debug) = debug_backtrace(false);
-	return Test::equals($expectation,$result,false,$debug["line"],$debug["file"]);
+if(!function_exists('neq')){
+	/**
+	 * $resultが$expectationが同じではない事を検証する
+	 * @param mixed $expectation ならないはずの値
+	 * @param mixed $result テスト対象の値
+	 * @return boolean
+	 */
+	function neq($expectation,$result){
+		list($debug) = debug_backtrace(false);
+		return Test::equals($expectation,$result,false,$debug["line"],$debug["file"]);
+	}
 }
-/**
- * テンポラリファイルを作成する
- * @param string $path
- * @param string $body
- */
-function ftmp($path,$body){
-	Test::ftmp($path,$body);
+if(!function_exists('ftmp')){
+	/**
+	 * テンポラリファイルを作成する
+	 * @param string $path
+	 * @param string $body
+	 */
+	function ftmp($path,$body){
+		Test::ftmp($path,$body);
+	}
 }
-/**
- * テンポラリファイルを保存するパスを返す
- * @return string
- */
-function tmp_path($path=null){
-	return Test::tmp_path($path);
+if(!function_exists('tmp_path')){
+	/**
+	 * テンポラリファイルを保存するパスを返す
+	 * @return string
+	 */
+	function tmp_path($path=null){
+		return Test::tmp_path($path);
+	}
 }
-/**
- * テスト用のHttpインスタンスを返す
- * @return Http
- */
-function test_browser(){
-	return Test::browser();
+if(!function_exists('test_browser')){
+	/**
+	 * テスト用のHttpインスタンスを返す
+	 * @return Http
+	 */
+	function test_browser(){
+		return Test::browser();
+	}
 }
-/**
- * mapに定義されたurlをフォーマットして返す
- * @param string $name
- * @return string
- */
-function test_map_url($name){
-	list($entry,$map_name) = (strpos($name,'::') !== false) ? explode('::',$name,2) : array(Test::current_entry(),$name);
-	$maps = Test::flow_output_maps($entry);
-	$args = func_get_args();
-	array_shift($args);
-	App::branch($entry);
-	if(isset($maps[$map_name][sizeof($args)])) return App::url(vsprintf($maps[$map_name][sizeof($args)],$args));
-	throw new RuntimeException($name.'['.sizeof($args).'] not found');
+if(!function_exists('test_map_url')){
+	/**
+	 * mapに定義されたurlをフォーマットして返す
+	 * @param string $name
+	 * @return string
+	 */
+	function test_map_url($name){
+		list($entry,$map_name) = (strpos($name,'::') !== false) ? explode('::',$name,2) : array(Test::current_entry(),$name);
+		$maps = Test::flow_output_maps($entry);
+		$args = func_get_args();
+		array_shift($args);
+		App::branch($entry);
+		if(isset($maps[$map_name][sizeof($args)])) return App::url(vsprintf($maps[$map_name][sizeof($args)],$args));
+		throw new RuntimeException($name.'['.sizeof($args).'] not found');
+	}
 }
-/**
- * テスト用Httpのアクセス結果を取得する
- * @param string $url
- * @param string $name
- * @return string
- */
-function handled_var($url,$name){
-	return Test::handled_var($url,$name);
+if(!function_exists('handled_var')){
+	/**
+	 * テスト用Httpのアクセス結果を取得する
+	 * @param string $url
+	 * @param string $name
+	 * @return string
+	 */
+	function handled_var($url,$name){
+		return Test::handled_var($url,$name);
+	}
+}
+if(!function_exists('b')){
+	/**
+	 * Httpリクエスト
+	 * @return Http
+	 */
+	function b(){
+		return new Http();
+	}
+}
+if(!function_exists('xml')){
+	/**
+	 * XMLで取得する
+	 * @param $xml 取得したXmlオブジェクトを格納する変数
+	 * @param $src 対象の文字列
+	 * @param $name ノード名
+	 * @return boolean
+	 */
+	function xml(&$xml,$src,$name=null){
+		return Tag::setof($xml,$src,$name);
+	}
 }
 /**
  * rhaco2の環境定義クラス
